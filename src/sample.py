52d579680601b536307073de4ed78417724d3c69
mailysg8 <mai.guedon@gmail.com>
2026-05-25 21:47:55 -0700

    Add samples created with latest (grouped) data

--- src/sample.py
+++ src/sample.py
@@ -28,12 +28,18 @@ def bootstrap_students(initial_data, target_students, seed = 42):
 
     return pd.concat(new_rows, ignore_index=True)
 
+# Read data
 initial_data = pd.read_excel('data/raw/final_data.xlsx', sheet_name='Student_Observations')
+grouped_data = pd.read_csv('data/processed/full_grouped_data.csv')
 
-# Create samples with 50 and 100 students
+# Create samples with 50 and 100 students for intial and grouped data
 sample_50 = bootstrap_students(initial_data, 50)
 sample_100 = bootstrap_students(initial_data, 100)
+grouped_sample_50 = bootstrap_students(grouped_data, 50)
+grouped_sample_100 = bootstrap_students(grouped_data, 100)
 
 # Save created samples
 sample_50.to_csv('data/sample/sample_50.csv')
 sample_100.to_csv('data/sample/sample_100.csv')
+grouped_sample_50.to_csv('data/sample/grouped_sample_50.csv')
+grouped_sample_100.to_csv('data/sample/grouped_sample_100.csv')
