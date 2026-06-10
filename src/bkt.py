52fdf4560e80ad9d37461522e49d264ab1fa2404
mailysg8 <mai.guedon@gmail.com>
2026-06-10 00:00:43 -0700

    Fix seed to ensure predictions are constitent

--- src/bkt.py
+++ src/bkt.py
@@ -1,9 +1,16 @@
 from pyBKT.models import Model
 from src.preprocess_split import preprocess 
+import random
+import numpy as np
 
 def bkt(data, kc_col = 'modeling_kc_id', seed=42, num_fits=10):
+    random.seed(seed)
+    np.random.seed(seed)
+
     processed_data = preprocess(data, kc_col)
+
     model = Model(seed = seed, num_fits = num_fits)
     model.fit(data = processed_data)
     predictions = model.predict(data=processed_data)
+
     return predictions
